02de412f9d28cd56f5a228f984bce5517356707e
Alastair Deng <adeng27@stanford.edu>
2024-02-16 21:09:23 -0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      FORTNITE BATTLE PASS by Alastair, Anna, Joseph, and Tanvir
+    </main>
+  );
+}
